36ca475db179bea8a33db7f9340694c74bb73993
LesBron <LesBron@users.noreply.github.com>
2020-08-24 15:24:41 +0100

    Update front-page.php
    Added text to contact us section.

--- front-page.php
+++ front-page.php
@@ -73,7 +73,7 @@ if($query->have_posts()): ?>
 
         <div class="contact__panel">
             <h2>Contact us</h2>
-            <p>You can email the Tools for Schools team any time.</p>
+            <p>You can email the Tools for Schools team any time. We work part time but aim to respond as soon as we can to all enquiries. </p>
             <a class="button" href="mailto:toolsforschools@westsussex.gov.uk">Email us</a>
         </div>
     </div>
